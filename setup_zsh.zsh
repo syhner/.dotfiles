a7ea215f3d13082ac8bca45109133126b17a15ed
Syhner <71605633+Syhner@users.noreply.github.com>
2022-09-16 21:00:41 +0200

    Remove change to default non-interactive shell

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -11,18 +11,11 @@ else
   echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
-if [ $"SHELL" = '/opt/homebrew/bin/zsh' ]; then
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
   echo '$SHELL is already /opt/homebrew/bin/zsh'
 else
   echo "Enter user password to change login shell"
   chsh -s /opt/homebrew/bin/zsh # -s newshell
-fi
-
-if sh --version | grep -q zsh; then
-  echo '/private/var/select/sh is already linked to /bin/zsh'
-else
-  echo "Enter superuser (sudo) password to symlink sh to preinstalled zsh"
-  sudo ln -fsv /bin/zsh /private/var/select/sh
 fi
 
 echo "A restart may be required for default shell changes to take effect"
