5967fe33e2b6b586bcba507edf9f0210403767a9
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2022-10-27 22:55:29 +0200

    script to open work apps

---
+++ open_work.sh
@@ -0,0 +1,7 @@
+# AppleScript: do shell script "~/.dotfiles/open_work.sh"
+
+open ~/Documents/Code/Work/main.code-workspace
+open -a "Google Chrome" --args --profile-directory="Work" # Profile name from "~/Library/Application Support/Google/Chrome/Work"
+open -a "Slack"
+open -a "pgAdmin 4"
+open -a "React Native Debugger"
