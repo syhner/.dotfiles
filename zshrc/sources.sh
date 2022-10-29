167e7204111f4536ec6dd85515a3349f96bbf132
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2022-10-29 11:16:22 +0200

    Autoload nvm

--- zshrc/sources.sh
+++ zshrc/sources.sh
@@ -5,6 +5,7 @@ source "$DOTFILES/load/spaceship.zsh"
 # Brews
 source /opt/homebrew/etc/profile.d/z.sh
 source /opt/homebrew/opt/nvm/nvm.sh # nvm
-source "$DOTFILES/load/ruby.zsh" # rbenv
+source ./load/nvm.zsh # Autoload nvm
+source ./load/ruby.zsh # rbenv
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh # zsh-autosuggestions
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # zsh-syntax-highlighting
