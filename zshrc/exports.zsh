c30ca8a0d3b01211ab03a6f054a3e5c29a3b86e4
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2022-12-31 13:20:08 +0100

    Add pnpm config

--- zshrc/exports.zsh
+++ zshrc/exports.zsh
@@ -7,6 +7,7 @@ export LOAD="$HOME/.dotfiles/zshrc/load"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'" # Syntax highlighting for man pages using bat
 export NULLCMD=bat # Use bat instead of cat
 export NVM_DIR="$HOME/.nvm" # https://github.com/nvm-sh/nvm#installing-and-updating
+export PNPM_HOME="$HOME/Library/pnpm"
 export PYENV_ROOT="$HOME/.pyenv" # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
 export RBENV_SHELL=zsh # rbenv
 export WORK="$HOME/Work/main.code-workspace"
--- zshrc/typesets.zsh
+++ zshrc/typesets.zsh
@@ -1,6 +1,7 @@
 typeset -U path
 path=(
   $GEM_HOME/bin
+  $PNPM_HOME
   $JAVA_HOME/bin
   $PYENV_ROOT/bin # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
   "/Users/siraj/.rbenv/shims" # rbenv
