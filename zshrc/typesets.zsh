336ccbd7b71d3842c8e3b2e4b92b4583ded2eb0e
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-01-18 10:48:59 +0100

    use $HOME for paths

--- zshrc/typesets.zsh
+++ zshrc/typesets.zsh
@@ -4,7 +4,7 @@ path=(
   $PNPM_HOME
   $JAVA_HOME/bin
   $PYENV_ROOT/bin # https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv
-  "/Users/siraj/.rbenv/shims" # rbenv
+  $HOME/.rbenv/shims # rbenv
   $path
   $ANDROID_SDK_ROOT/emulator # https://reactnative.dev/docs/environment-setup
   $ANDROID_SDK_ROOT/platform-tools # https://reactnative.dev/docs/environment-setup
