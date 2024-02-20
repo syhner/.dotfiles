f334e327ebe18d476a0f915676144f3faaa86eec
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2024-02-20 20:26:31 +0100

    move mac setup to it's own directory

--- open/work.zsh
+++
@@ -1,5 +0,0 @@
-# AppleScript: do shell script "~/.dotfiles/open/work.sh"
-
-open ~/work/main.code-workspace
-open -a "Google Chrome" --args --profile-directory="work" # Profile name from "~/Library/Application Support/Google/Chrome/work"
-open -a "Slack"
