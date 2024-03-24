f78a03d6244aa2edb54f8cf6cdcbd5eed037257d
syhner <71605633+syhner@users.noreply.github.com>
2024-03-25 00:52:02 +0100

    add aliases

--- zshrc/aliases.zsh
+++ zshrc/aliases.zsh
@@ -5,6 +5,7 @@ alias brewcasks="brew ls --casks | parallel \"echo -n 'cask \"{}\" #'; brew desc
 alias brewdump='brew bundle dump --describe --file=~/.dotfiles/Brewfile.dump --force'
 alias brewformulae="brew leaves --installed-on-request | parallel \"echo -n 'brew \"{}\" #'; brew desc --formula --eval-all {} | cut -d ':' -f 2-\""
 alias cmd='command'
+alias cwd='pwd | pbcopy'
 alias exitcode='echo $?'
 alias ftrail='<<<${(F)fpath}'
 
@@ -21,6 +22,7 @@ alias ls='exa --across --all --classify --git --group-directories-first'
 alias mkdir='mkdir -p'
 alias myip='ipconfig getifaddr en0'
 alias pi='ssh raspberrypi.local -l pi'
+alias pnpx='pnpm dlx'
 alias expose-cf='cloudflared tunnel run local'
 alias expose-ng="ngrok --domain=$NGROK_DOMAIN"               # expose-ng http 3000
 alias expose-ng-edge="ngrok tunnel --label edge=$NGROK_EDGE" # expose-ng-edge 3000
